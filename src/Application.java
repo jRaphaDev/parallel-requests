0f5fa3bcae320f5eae7d3b30159257422825a519
Raphael Freitas <dev.rapha@gmail.com>
2020-05-05 20:15:27 -0300

    change to stream callable

--- src/Application.java
+++ src/Application.java
@@ -12,5 +12,4 @@ public class Application {
         System.out.println(resultado);
     }
 
-
 }
--- src/ServicoConsulta.java
+++ src/ServicoConsulta.java
@@ -1,18 +1,37 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.stream.Collectors;
 
 public class ServicoConsulta {
 
     public List<String> consultarDados(){
+        List<Callable<String>> list = getRequestsList();
 
-        Proxy proxyUm = new PrimeiroProxyConsulta();
-        Proxy proxyDois = new SegundoProxyConsulta();
 
-        List<String> resultado = Arrays.asList(proxyUm.doRequest(), proxyDois.doRequest())
-                .parallelStream()
+        List<String> resultado = list.parallelStream()
+                .map(callable -> {
+                    try {
+                        System.out.format("callable: %s [%s]\n", callable, Thread.currentThread().getName());
+                        return callable.call();
+                    } catch (Exception e) {
+
+                        e.printStackTrace();
+                        return "";
+                    }
+                })
                 .collect(Collectors.toList());
 
         return resultado;
+    }
+
+    private List<Callable<String>> getRequestsList() {
+
+        Callable<String> callable1 = () -> new PrimeiroProxyConsulta().doRequest();
+        Callable<String> callable2 = () -> new SegundoProxyConsulta().doRequest();
+
+        List<Callable<String>> list = Arrays.asList(callable1, callable2);
+
+        return list;
     }
 }
